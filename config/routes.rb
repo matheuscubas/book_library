6ba7c2555de97d001a075fe496f0187d570bb353
matheus <86804637+matheuscubas@users.noreply.github.com>
2023-09-20 22:13:25 -0300

    🎉 Add basic project structure and first gems

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
