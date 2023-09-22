d0fc15418b60ec937f3910590bb263e359af5f44
matheus <86804637+matheuscubas@users.noreply.github.com>
2023-09-22 16:18:15 -0300

    🩹 Fix declaration of expected result

--- spec/requests/api/books_spec.rb
+++ spec/requests/api/books_spec.rb
@@ -13,11 +13,11 @@ RSpec.describe 'Api::Books', type: :request do
         parameter name: :author, in: :query, type: :string, required: false
         parameter name: :genre, in: :query, type: :string, required: false
 
-        before do
-          create_list(:book, 5)
-        end
-
         response(200, 'Return a list of books') do
+          before do
+            create_list(:book, 5)
+          end
+
           run_test! do |request|
             body = JSON.parse(request.body, symbolize_names: true)
             expect(response).to have_http_status(200)
@@ -35,7 +35,11 @@ RSpec.describe 'Api::Books', type: :request do
           end
 
           context 'Return just the books that has the same genre' do
-            let(:my_book) { create(:book, genre: 'Impossible to exist') }
+            before do
+              create(:book, genre: 'Impossible to exist')
+            end
+
+            let(:my_book) { Book.last }
             let(:genre) { 'Impossible to exist' }
 
             run_test! do |request|
@@ -43,12 +47,16 @@ RSpec.describe 'Api::Books', type: :request do
               expect(response).to have_http_status(200)
               expect(body.keys).to eq([:books])
               expect(Book.count).not_to eq(1)
-              expect(body[:books].first).to eq(my_book)
+              expect(body[:books].first[:id]).to eq(my_book.id)
             end
           end
 
           context 'Return just the books that has the same author' do
-            let(:my_book) { create(:book, author: 'Impossible to exist') }
+            before do
+              create(:book, author: 'Impossible to exist')
+            end
+
+            let(:my_book) { Book.last }
             let(:author) { 'Impossible to exist' }
 
             run_test! do |request|
@@ -56,12 +64,16 @@ RSpec.describe 'Api::Books', type: :request do
               expect(response).to have_http_status(200)
               expect(body.keys).to eq([:books])
               expect(Book.count).not_to eq(1)
-              expect(body[:books].first).to eq(my_book)
+              expect(body[:books].first[:id]).to eq(my_book.id)
             end
           end
 
           context 'Return just the books that has the same title' do
-            let(:my_book) { create(:book, title: 'Impossible to exist') }
+            before do
+              create(:book, title: 'Impossible to exist')
+            end
+
+            let(:my_book) { Book.last }
             let(:title) { 'Impossible to exist' }
 
             run_test! do |request|
@@ -69,7 +81,7 @@ RSpec.describe 'Api::Books', type: :request do
               expect(response).to have_http_status(200)
               expect(body.keys).to eq([:books])
               expect(Book.count).not_to eq(1)
-              expect(body[:books].first).to eq(my_book)
+              expect(body[:books].first[:id]).to eq(my_book.id)
             end
           end
 
