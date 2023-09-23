3b3be13778016ee258b2ce2b39f71f9631fefa92
matheus <86804637+matheuscubas@users.noreply.github.com>
2023-09-23 12:38:27 -0300

    ✨ Add form object to handle validation and persistence logic

---
+++ app/forms/create_book_form.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class CreateBookForm
+  include ActiveModel::Model
+
+  attr_accessor :title, :genre, :author, :publication_year
+
+  validates :title, :genre, :author, presence: true
+  validates :title, :genre, :author, length: { minimum: 2 }
+  validates :publication_year, comparison: { less_than_or_equal_to: Date.today.year }
+  validate  :uniqueness
+
+  def persist
+    return false unless valid?
+
+    Book.new(title:, genre:, author:, publication_year:).save
+  end
+
+  private
+
+  def uniqueness
+    return true unless Book.where(title:, publication_year:, author:).any?
+
+    errors.add(:uniqueness, message: "#{title} already been recomended before.")
+    false
+  end
+end
