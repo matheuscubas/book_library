f13b7a0c85fc711707697d37cabf408813bddd25
matheus <86804637+matheuscubas@users.noreply.github.com>
2023-09-22 21:43:40 -0300

    ✨ Add create book endpoint

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -7,12 +7,24 @@ module Api
       render result
     end
 
-    def create; end
+    def create
+      book = Book.new(create_params)
+
+      if book.save
+        render json: { book: }
+      else
+        render status: :unprocessable_entity, json: { error: book.errors.messages }
+      end
+    end
 
     private
 
     def query_params
       params.permit(:title, :genre, :author)
+    end
+
+    def create_params
+      params.permit(:title, :genre, :author, :publication_year)
     end
   end
 end
